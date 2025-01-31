c6c420558d26277259c2eadfbeb5e09de8ab39ef
Aaron Klump <sourcecode@intheloftstudios.com>
2025-01-31 10:46:36 -0800

    Remove mkdir.

--- inc/bootstrap.sh
+++ inc/bootstrap.sh
@@ -4,4 +4,3 @@ s="${BASH_SOURCE[0]}";[[ "$s" ]] || s="${(%):-%N}";while [ -h "$s" ];do d="$(cd 
 source "$__DIR__/../config.sh"
 
 QUEUE_FILE="$__DIR__/../.cache/publish"
-mkdir -p "$(dirname "$QUEUE_FILE")"
